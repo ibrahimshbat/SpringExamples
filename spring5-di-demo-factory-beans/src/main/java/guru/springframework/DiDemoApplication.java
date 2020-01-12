756431cbffb7ffe60ccef05d6a05ecb2e9d4d65f
Ibrahim Elsanosi <ibrahimsabat@yahoo.com>
2020-01-12 22:42:57 +0000

    add new example spring5-di-demo-property-source with file data

---
+++ spring5-di-demo-factory-beans/src/main/java/guru/springframework/DiDemoApplication.java
@@ -0,0 +1,26 @@
+package guru.springframework;
+
+import guru.springframework.controllers.ConstructorInjectedController;
+import guru.springframework.controllers.GetterInjectedController;
+import guru.springframework.controllers.MyController;
+import guru.springframework.controllers.PropertyInjectedController;
+import guru.springframework.examplebeans.FakeDataSource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+
+		MyController controller = (MyController) ctx.getBean("myController");
+
+		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+
+		System.out.println(fakeDataSource.getUser());
+
+
+	}
+}
